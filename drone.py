80aa1c097216c5f1ad30a83061aa94c76e63c961
bindi-nagda <nagdabindi@gmail.com>
2022-12-24 16:58:50 -0500

    add pre and post-physics steps

--- drone.py
+++ drone.py
@@ -1,14 +1,17 @@
-import os 
-from isaacgym import gymapi
-from isaacgym import gymutil
-from isaacgym import gymtorch
-from isaacgym.torch_utils import *
-from base.vec_task import VecTask
-
+import os
 import math
 import torch
 import random 
 import numpy as np
+
+from isaacgym import gymapi
+from isaacgym import gymutil
+from isaacgym import gymtorch
+from isaacgym.torch_utils import *
+
+# local includes
+from base.vec_task import VecTask
+from utils.torch_jit_utils import *
 
 class drone():
 
@@ -27,14 +30,12 @@ class drone():
 
         super().__init__(config=self.cfg, rl_device=rl_device, sim_device=sim_device, graphics_device_id=graphics_device_id, headless=headless, virtual_screen_capture=virtual_screen_capture, force_render=force_render)
         
-        dofs_per_env = 4
-
         # get gym GPU state tensors
-        _root_tensor = self.gym.acquire_actor_root_state_tensor(self.sim) # drone, marker, drone, marker....
-        _dof_states = self.gym.acquire_dof_state_tensor(self.sim)
+        self.root_tensor = self.gym.acquire_actor_root_state_tensor(self.sim) # drone, marker, drone, marker....
+        self.dof_states = self.gym.acquire_dof_state_tensor(self.sim)
 
-        vec_root_tensor = gymtorch.wrap_tensor(_root_tensor).view(self.num_envs, 2, 13)
-        vec_dof_tensor = gymtorch.wrap_tensor(_dof_states) #.view(self.num_envs, dofs_per_env,2)
+        vec_root_tensor = gymtorch.wrap_tensor(self.root_tensor).view(self.num_envs, 2, 13)
+        vec_dof_tensor = gymtorch.wrap_tensor(self.dof_states) #.view(self.num_envs, self.num_dofs,2)
 
         self.drone_states = vec_root_tensor[:, 0, :]
         self.drone_positions = self.drone_states[:, 0:3]
@@ -59,6 +60,10 @@ class drone():
         max_thrust = 2
         self.thrust_lower_limits = torch.zeros(4, device=self.device, dtype=torch.float32)
         self.thrust_upper_limits = max_thrust * torch.ones(4, device=self.device, dtype=torch.float32)
+
+        # control tensors
+        self.thrusts = torch.zeros((self.num_envs, 2, 3), dtype=torch.float32, device=self.device, requires_grad=False)
+        self.forces = torch.zeros((self.num_envs, self.num_bodies, 3), dtype=torch.float32, device=self.device, requires_grad=False)
 
         self.all_actor_indices = torch.arange(self.num_envs * 2, dtype=torch.int32, device=self.device).reshape((self.num_envs, 2)) # drone and target
 
@@ -102,6 +107,7 @@ class drone():
         asset_options.fix_base_link = False
         asset_options.flip_visual_attachments = False
         holybro = self.gym.load_asset(self.sim, asset_root, holybro_asset_file, asset_options)
+        self.num_bodies = self.gym.get_asset_rigid_body_count(holybro)
 
         # sphere
         asset_options.fix_base_link = True
@@ -198,7 +204,117 @@ class drone():
 
         return torch.unique(torch.cat([target_actor_indices, actor_indices]))
 
+    def pre_physics_step(self, _actions):
 
-        
+       # resets
+        set_target_ids = (self.progress_buf % 500 == 0).nonzero(as_tuple=False).squeeze(-1)
+        target_actor_indices = torch.tensor([], device=self.device, dtype=torch.int32)
+        if len(set_target_ids) > 0:
+            target_actor_indices = self.set_targets(set_target_ids)
 
+        reset_env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
+        actor_indices = torch.tensor([], device=self.device, dtype=torch.int32)
+        if len(reset_env_ids) > 0:
+            actor_indices = self.reset_idx(reset_env_ids)
+
+        reset_indices = torch.unique(torch.cat([target_actor_indices, actor_indices]))
+        if len(reset_indices) > 0:
+            self.gym.set_actor_root_state_tensor_indexed(self.sim, self.root_tensor, gymtorch.unwrap_tensor(reset_indices), len(reset_indices))
+
+        actions = _actions.to(self.device)
+
+        thrust_action_speed_scale = 200
+        self.thrusts += self.dt * thrust_action_speed_scale * actions[:, 8:12]
+        self.thrusts[:] = tensor_clamp(self.thrusts, self.thrust_lower_limits, self.thrust_upper_limits)
+
+        self.forces[:, 1, 2] = self.thrusts[:, 0]
+        self.forces[:, 2, 2] = self.thrusts[:, 1]
+        self.forces[:, 3, 2] = self.thrusts[:, 2]
+        self.forces[:, 4, 2] = self.thrusts[:, 3]
+
+        # clear actions for reset envs
+        self.thrusts[reset_env_ids] = 0.0
+        self.forces[reset_env_ids] = 0.0
+
+        # apply actions
+        self.gym.apply_rigid_body_force_tensors(self.sim, gymtorch.unwrap_tensor(self.forces), None, gymapi.LOCAL_SPACE)
+
+    def post_physics_step(self):
+
+        self.progress_buf += 1
+
+        self.gym.refresh_actor_root_state_tensor(self.sim)
+        self.gym.refresh_dof_state_tensor(self.sim)
+
+        self.compute_observations()
+        self.compute_reward()
+
+        if self.viewer and self.debug_viz:
+            # compute start and end positions for visualizing thrust lines
+            self.gym.refresh_rigid_body_state_tensor(self.sim)
+            rotor_indices = torch.LongTensor([2, 4, 6, 8])
+            quats = self.rb_quats[:, rotor_indices]
+            dirs = -quat_axis(quats.view(self.num_envs * 4, 4), 2).view(self.num_envs, 4, 3)
+            starts = self.rb_positions[:, rotor_indices] + self.rotor_env_offsets
+            ends = starts + 0.1 * self.thrusts.view(self.num_envs, 4, 1) * dirs
+
+            # submit debug line geometry
+            verts = torch.stack([starts, ends], dim=2).cpu().numpy()
+            colors = np.zeros((self.num_envs * 4, 3), dtype=np.float32)
+            colors[..., 0] = 1.0
+            self.gym.clear_lines(self.viewer)
+            self.gym.add_lines(self.viewer, None, self.num_envs * 4, verts, colors)
+
+    def compute_observations(self):
+        self.obs_buf[..., 0:3] = (self.target_root_positions - self.root_positions) / 3
+        self.obs_buf[..., 3:7] = self.root_quats
+        self.obs_buf[..., 7:10] = self.root_linvels / 2
+        self.obs_buf[..., 10:13] = self.root_angvels / math.pi
+        return self.obs_buf
+
+    def compute_reward(self):
+        self.rew_buf[:], self.reset_buf[:] = compute_drone_reward(
+            self.root_positions,
+            self.target_root_positions,
+            self.root_quats,
+            self.root_linvels,
+            self.root_angvels,
+            self.reset_buf, self.progress_buf, self.max_episode_length
+        )
+
+#####################################################################
+###=========================jit functions=========================###
+#####################################################################
+
+@torch.jit.script
+def compute_drone_reward(root_positions, target_root_positions, root_quats, root_linvels, root_angvels, reset_buf, progress_buf, max_episode_length):
+    # type: (Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, float) -> Tuple[Tensor, Tensor]
+
+    # distance to target
+    target_dist = torch.sqrt(torch.square(target_root_positions - root_positions).sum(-1))
+    pos_reward = 1.0 / (1.0 + target_dist * target_dist)
+
+    # uprightness
+    ups = quat_axis(root_quats, 2)
+    tiltage = torch.abs(1 - ups[..., 2])
+    up_reward = 5.0 / (1.0 + tiltage * tiltage)
+
+    # spinning
+    spinnage = torch.abs(root_angvels[..., 2])
+    spinnage_reward = 1.0 / (1.0 + spinnage * spinnage)
+
+    # combined reward
+    # uprigness and spinning only matter when close to the target
+    reward = pos_reward + pos_reward * (up_reward + spinnage_reward)
+
+    # resets due to misbehavior
+    ones = torch.ones_like(reset_buf)
+    die = torch.zeros_like(reset_buf)
+    die = torch.where(target_dist > 8.0, ones, die)
+    die = torch.where(root_positions[..., 2] < 0.5, ones, die)
+
+    # resets due to episode length
+    reset = torch.where(progress_buf >= max_episode_length - 1, ones, die)
+
+    return reward, reset
 
